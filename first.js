66c01a4b8feab79e79dd0a1f8774e4da24ded83e
[Riya Bargoda] <riyabargoda07@gamil.com>
2025-07-17 23:05:38 +0530

    iamge added

--- first.js
+++ first.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-let our_service = document.querySelector("#button1")
-our_service.addEventListener("click", () => {
-    let Fun = document.querySelector(".our_services")
-    console.log("button was clicked");
-
-
-})
-
-let consult = document.querySelector("#button2")
-consult.onclick = () => {
-    console.log("button was clicked");
-}
-
-let talk = document.querySelector("#button3")
-talk.onclick = () => {
-    console.log("button was clicked");
-=======
-
-
 
 let our_service = document.querySelector("#button1")
 our_service.addEventListener("click", () => {
@@ -35,5 +15,4 @@ consult.onclick = () => {
 let talk = document.querySelector("#button3")
 talk.onclick = () => {
     console.log("button was clicked");
->>>>>>> cdd5466777a5a379a079371792217ae34834a204
 }
