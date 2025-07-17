0d9586ad15b4f93588e60f931ef4b60041572078
[Riya Bargoda] <riyabargoda07@gamil.com>
2025-07-17 21:43:28 +0530

    desgin completed

---
+++ first.js
@@ -0,0 +1,17 @@
+let our_service = document.querySelector("#button1")
+our_service.addEventListener("click", () => {
+    let Fun = document.querySelector(".our_services")
+    console.log("button was clicked");
+
+
+})
+
+let consult = document.querySelector("#button2")
+consult.onclick = () => {
+    console.log("button was clicked");
+}
+
+let talk = document.querySelector("#button3")
+talk.onclick = () => {
+    console.log("button was clicked");
+}
